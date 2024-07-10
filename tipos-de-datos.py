c6ecc462e031225616feb3ada58278e8917ad6d4
AlvaroRiascos23 <alva4ro@live.com>
2024-07-10 18:54:55 -0500

    se inicializo el repo de python

---
+++ tipos-de-datos.py
@@ -0,0 +1,15 @@
+"string" #se utiliza para tipos de datos texto
+'string'
+"""string"""
+'''string'''
+"""Tus datos son
+    nombre: Alvaro
+    Apellido: Galvis"""
+    #tipos de datos numericos
+4#tipo de dato entero
+4.5#tipo de dato flotante
+
+#tipos de datos booleanos
+True # True va con T mayuscula
+False #y False va con F mayuscula
+
