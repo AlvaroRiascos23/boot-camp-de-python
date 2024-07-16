9a941a99967f81cf0070c4421566173729e1f143
AlvaroRiascos23 <alva4ro@live.com>
2024-07-15 19:53:27 -0500

    se abordaron fundamentos de python

--- tipos-de-datos.py
+++ tipos-de-datos.py
@@ -1,15 +1,17 @@
 "string" #se utiliza para tipos de datos texto
-'string'
+'string' 
 """string"""
 '''string'''
 """Tus datos son
-    nombre: Alvaro
-    Apellido: Galvis"""
-    #tipos de datos numericos
+    nombre: Jheyson
+    apellido: Galvis"""
+#tipos de datos numericos
 4#tipo de dato entero
 4.5#tipo de dato flotante
 
 #tipos de datos booleanos
-True # True va con T mayuscula
-False #y False va con F mayuscula
+True#True va con T mayúscula
+False#False va con F mayúscula
 
+
+    
