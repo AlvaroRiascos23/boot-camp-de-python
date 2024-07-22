9f7295d79abbba4af3dae2620d14ea94bb1646cd
AlvaroRiascos23 <alva4ro@live.com>
2024-07-22 18:52:37 -0500

    Operadores aritmeticos

--- datos_compuestos.py
+++ datos_compuestos.py
@@ -1,4 +1,4 @@
-lista = ["Alvaro Riascos", "tecnotutoriales Jheyson", True, 1.69]
+lista = ["Alvaro_Riascos", "tecnotutoriales Jheyson", True, 1.69]
 print(lista[1])
 lista[3] = "Alvarito"
 print(lista[3])
@@ -10,7 +10,7 @@ print(tupla[1])
 
 #creando conjunto o set
 #Un conjunto no admite elementos duplicados
-conjunto = {"Alvaro Riascos","tecnotutoriales Jheyson Galvis", True, 1.69,1.69}
+conjunto = {"Alvaro Riascos","tecnotutoriales Jheyson Galvis", True, 1.69}
 print(conjunto)
 
 #Creando un diccionario
@@ -24,3 +24,7 @@ print(diccionario["esta feliz"])
 print(diccionario["nombre"])
 print(diccionario["apellido"])
 print(diccionario["estatura"])
+nombre = input("Digite su nombre ")
+nombre_uno = int(nombre)
+
+print(type(nombre_uno))
